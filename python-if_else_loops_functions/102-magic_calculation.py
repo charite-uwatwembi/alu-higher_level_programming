d72247d539c18bbac966ff16f79f634582695cca
charite-uwatwembi <c.uwatwembi@alustudent.com>
2023-02-08 01:06:21 -0800

    102-magic_calculation.py

--- python-if_else_loops_functions/102-magic_calculation.py
+++ python-if_else_loops_functions/102-magic_calculation.py
@@ -1,10 +1,3 @@
-#!/usr/bin/python3
-# 102-magic_calculation.py
-
-
-def magic_calculation(a, b, c):
-    """Match bytecode provided by Holberton School."""
-        if a < b:
-            return (c)
-        if c > b:
-            return (a + b)
+#!/usr/bin/pyth0n3
+def magic_calculation(a, b):
+    return (98 + a ** b)
