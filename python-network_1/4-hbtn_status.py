5301c5480cdfc37f25c6faa0b01ae3429aa05c87
charite-uwatwembi <c.uwatwembi@alustudent.com>
2023-04-05 10:18:57 +0000

    4-hbtn_status.py

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -8,4 +8,4 @@ import requests
 if __name__ == "__main__":
     r = requests.get('https://intranet.hbtn.io/status')
     t = r.text
-    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t)))
+    print('Body response:\n\t- type: {}\n\t- content: {}'.format(type(t), t))
