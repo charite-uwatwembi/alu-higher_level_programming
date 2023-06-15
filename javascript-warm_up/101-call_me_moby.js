f3e5018691619977aca2357139f9dc34bd1ad5a3
charite-uwatwembi <c.uwatwembi@alustudent.com>
2023-06-15 05:09:26 -0700

    101-call_me_moby.js

--- javascript-warm_up/101-call_me_moby.js
+++ javascript-warm_up/101-call_me_moby.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
-function callMeMoby (x, theFunction) {
-    for (let i = 0; i < x; i++) {
-      theFunction();
-    }
+exports.callMeMoby = function (x, theFunction) {
+  for (let i = 1; i <= x; i++) {
+    theFunction();
   }
-  module.exports = { callMeMoby }; 
+};
