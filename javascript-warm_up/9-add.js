b86d49e5550643b75add6ca5ec3aa79601ca915e
charite-uwatwembi <c.uwatwembi@alustudent.com>
2023-06-15 04:31:48 -0700

    9-add.js

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,13 +1,8 @@
 #!/usr/bin/node
-const num1 = parseInt(process.argv[2]);
-const num2 = parseInt(process.argv[3]);
-
-if (isNaN(num1) || isNaN(num2)) {
-  console.log('NaN');
-} else {
-  console.log(add(num1, num2));
-}
-
+const a = parseInt(process.argv[2]);
+const b = parseInt(process.argv[3]);
 function add (a, b) {
   return a + b;
-} 
+}
+
+console.log(add(a, b));
