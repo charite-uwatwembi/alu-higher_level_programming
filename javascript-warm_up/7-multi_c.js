04fd2308d36a1282f85ea34191aa0dfedcb9386e
charite-uwatwembi <c.uwatwembi@alustudent.com>
2023-06-15 04:41:33 -0700

    7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,10 +1,5 @@
 #!/usr/bin/node
-const arg = process.argv.slice(2);
-const c = parseInt(arg[0]);
-if (c === undefined || isNaN(c)) {
-  console.log('Missing number of occurences');
-} else {
-  for (let i = 1; i <= c; i++) {
-    console.log('C is fun');
-    }
+const firstArgv = process.argv[2];
+if (isNaN(firstArgv)) { console.log('Missing number of occurrences'); } else {
+  for (let i = 0; i < firstArgv; i++) { console.log('C is fun'); }
 }
